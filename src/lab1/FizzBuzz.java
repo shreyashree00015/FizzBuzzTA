3c6be03dc965c3f599dbb009cda804783e23f16e
Shreya Shree <shreyashree00015@gmail.com>
2025-09-08 12:29:40 -0400

    while loop refactoring

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,8 +6,8 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
+        int i=1;
+        while( i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,8 +29,8 @@ class FizzBuzz {
             } else {
                 //final results
                 System.out.println(i);
-
             }
+            i++;
         }
     }
 }
